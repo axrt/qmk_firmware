d28a953cd27f50cd304292d538906abfcc10bab2
Alexander Tuzhikov <alexander.tuzhikov@gmail.com>
2019-07-01 16:43:33 -0700

    Add lang change key;

--- keyboards/ergodox_ez/keymaps/default_osx_alext/keymap.c
+++ keyboards/ergodox_ez/keymaps/default_osx_alext/keymap.c
@@ -45,14 +45,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Right|           | Down |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * |--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- * |MO(MOTI)| Ctrl | Alt  | CMD  |AVAIL |                                       | Space|   `  |   [  |   ]  | Neovim |
+ * |MO(MOTI)| Ctrl | Alt  | CMD  | Space|                                       | Space|   `  |   [  |   ]  | Neovim |
  * `------------------------------------'                                       `------------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | AVAIL| AVAIL|       |TLWind|TRWind|
+ *                                        | Home | End  |       |TLWind|TRWind|
  *                                 ,------|------|------|       |------+-------+------.
- *                                 |      |      | AVAIL|       |TUSess|       |      |
- *                                 | Enter| AVAIL|------|       |------| AVAIL | AVAIL|
- *                                 |      |      | AVAIL|       |TLSess|       |      |
+ *                                 |      |      | PgUp |       |TUSess|       |      |
+ *                                 | Enter| Lang |------|       |------| AVAIL | AVAIL|
+ *                                 |      |      | PgDn |       |TLSess|       |      |
  *                                 `--------------------'       `---------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -63,12 +63,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,             KC_Q,              KC_W,        KC_E,          KC_R,       KC_T,       KC_LEFT,
       KC_LCTRL,           KC_A,              KC_S,        KC_D,          KC_F,       KC_G,
       KC_LSFT,            KC_Z,              KC_X,        KC_C,          KC_V,       KC_B,       KC_RIGHT,
-      MO(MOTI),           KC_LCTRL,          KC_LALT,     KC_LCMD,       KC_NO,
-      KC_NO,              KC_NO,
-      KC_NO,
-      KC_ENT,             KC_NO,             KC_NO,
+      MO(MOTI),           KC_LCTRL,          KC_LALT,     KC_LCMD,       KC_SPC,
+      KC_HOME,            KC_END,
+      KC_PGUP,
+      KC_ENT,             LCMD(KC_SPC),      KC_PGDN,
       // right hand
-      KC_7,               KC_8,              KC_9,        KC_0,          KC_MINS,    KC_EQL,    KC_BSPC,
+      KC_7,               KC_8,              KC_9,        KC_0,          KC_MINS,    KC_EQL,     KC_BSPC,
       KC_UP,              KC_Y,              KC_U,        KC_I,          KC_O,       KC_P,       KC_BSLS,
       KC_H,               KC_J,              KC_K,        KC_L,          KC_SCLN,    KC_QUOT,
       KC_DOWN,            KC_N,              KC_M,        KC_COMM,       KC_DOT,     KC_SLSH,    KC_RSFT,
