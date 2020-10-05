647e0365a4bfc2cd7eaebdfecbc254f10b1a5ef7
Alex Tuzhikov <atuzhikov@guardanthealth.com>
2020-10-05 11:20:11 -0700

    Increase mouse speed a little;

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -42,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_EXPANDER_ROW_PINS { 0, 1, 2, 3, 4, 5, 6 }
 
 
-#define MOUSEKEY_INTERVAL       20
+#define MOUSEKEY_INTERVAL       10
 #define MOUSEKEY_DELAY          0
 #define MOUSEKEY_TIME_TO_MAX    60
 #define MOUSEKEY_MAX_SPEED      10
