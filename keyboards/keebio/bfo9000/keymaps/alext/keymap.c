172a169af6a50f467d6b7b1556c9d7c5f0e4b491
Alex Tuzhikov <atuzhikov@guardanthealth.com>
2020-10-05 11:39:58 -0700

    Add some bfo improvements;

--- keyboards/keebio/bfo9000/keymaps/alext/keymap.c
+++ keyboards/keebio/bfo9000/keymaps/alext/keymap.c
@@ -38,11 +38,11 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [_BASE] = LAYOUT(                                                                                                                       \
       OPEN_HEADER,  OPEN_SOURCE, G_STATUS,       GIT_PUSH_GH, GIT_PUSH_GH_MASTER, ASYNC_TASK,       SPACE_LEFT, SPACE_RIGHT, PREFERENCES,  \
-      TEST_NEAREST, KC_N,        ULTISNIPS_EDIT, GIT_PUSH_BB, GIT_PUSH_BB_MASTER, ASYNC_TASK_EDIT,  KC_N,       KC_N,        PRINT_SCREEN, \
+      TEST_NEAREST, KC_N,        ULTISNIPS_EDIT, GIT_PUSH_BB, GIT_PUSH_BB_MASTER, ASYNC_TASK_EDIT,  KC_SLASH,        KC_BSPC,        PRINT_SCREEN, \
       COC_CONFIG,   KC_N,        KC_N,           KC_N,        KC_N,               KC_N,             KC_7,       KC_8,        KC_9,         \
-      KC_N,         KC_N,        KC_N,           KC_N,        KC_N,               KC_N,             KC_4,       KC_5,        KC_6,         \
-      CHROME,       POMODORO,    CALIBRE,        FINDER,      DOWNLOADS,          DOCUMENTS,        KC_1,       KC_2,        KC_3,         \
-      SPARK,        FANTASTICAL, GIT_KRAKEN,     MARGIN_NOTE, OMNIFOCUS,          ACTIVITY_MONITOR, KC_0,       DOUBLE_ZERO, TRIPLE_ZERO   \
+      DOCUMENTS,    ACTIVITY_MONITOR,            KC_N,           KC_N,            KC_N,               KC_N,             KC_4,       KC_5,        KC_6,         \
+      CHROME,       POMODORO,    CALIBRE,        FINDER,      DOWNLOADS,          KC_ENTER,        KC_1,       KC_2,        KC_3,         \
+      SPARK,        FANTASTICAL, GIT_KRAKEN,     MARGIN_NOTE, OMNIFOCUS,          KC_DOT,          KC_0,       DOUBLE_ZERO, TRIPLE_ZERO   \
       )
 };
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
